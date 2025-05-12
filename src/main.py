ef8c721859cb7b6a40751fa30eeea68393d6fb85
Francisco Balmaceda <franciscobalmaceda@Franciscos-MacBook-Air.local>
2025-05-11 20:19:23 -0400

    add linting

--- src/main.py
+++ src/main.py
@@ -8,15 +8,14 @@ load_dotenv()
 gemini_api = os.getenv("GEMINI_API")
 gemini_client = genai.Client(api_key=gemini_api)
 
-
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     response = gemini_client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents="Explain how AI works in a few words",
+        model="gemini-2.0-flash",
+        contents="Explain how AI works in a few words",
     )
     return {"Hello": response.text}
 
