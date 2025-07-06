c31b94ff5710363d5da7ce00a9a957ada84b12f8
josefinareyes <josefinareyes@uc.cl>
2025-07-06 13:22:53 -0400

    fix bug

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -7,12 +7,6 @@ from apps.auth.authService import AuthService
 router = APIRouter(prefix="/auth", tags=["auth"])
 service = AuthService()
 
-
-@router.post("/register", response_model=UserOut, status_code=status.HTTP_201_CREATED)
-async def register_user(user_in: UserCreate):
-    return await service.register_user(user_in)
-
-
 @router.post("/login", response_model=Token)
 async def login(form_data: Dict[str, str]):
     return await service.login_for_access_token(form_data)
